1ac8761ab341afcaf1c067e042399b4eba94789a
Abel Masila <abelmasila@gmail.com>
2020-09-22 00:02:39 +0300

    computed

--- main.js
+++ main.js
@@ -2,12 +2,24 @@ const app = Vue.createApp({
   data() {
     return {
       product: "Boots",
-      image: "./assets/images/socks_green.jpg",
+      selectedVariant: 0,
       inventory: 100,
+      brand: "Nike",
+      product: "Socks",
       details: ["50% cotton", "30% wool", "20% polyester"],
       variants: [
-        { id: 2234, color: "green", image: "./assets/images/socks_green.jpg" },
-        { id: 2235, color: "blue", image: "./assets/images/socks_blue.jpg" },
+        {
+          id: 2234,
+          color: "green",
+          image: "./assets/images/socks_green.jpg",
+          qty: 40,
+        },
+        {
+          id: 2235,
+          color: "blue",
+          image: "./assets/images/socks_blue.jpg",
+          qty: 0,
+        },
       ],
       cart: 0,
     };
@@ -16,8 +28,19 @@ const app = Vue.createApp({
     addToCart() {
       this.cart += 1;
     },
-    updateImage(vImage) {
-      this.image = vImage;
+    updateVariant(index) {
+      this.selectedVariant = index;
+    },
+  },
+  computed: {
+    title() {
+      return this.brand + " " + this.product;
+    },
+    image() {
+      return this.variants[this.selectedVariant].image;
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].qty;
     },
   },
 });
