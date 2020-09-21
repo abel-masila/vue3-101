9f7cd53a9c9d526d459fc6f0b223ba7f50e76758
Abel Masila <abelmasila@gmail.com>
2020-09-21 23:40:42 +0300

    event listers

--- main.js
+++ main.js
@@ -6,10 +6,18 @@ const app = Vue.createApp({
       inventory: 100,
       details: ["50% cotton", "30% wool", "20% polyester"],
       variants: [
-        { id: 2234, color: "green" },
-        { id: 2235, color: "blue" },
+        { id: 2234, color: "green", image: "./assets/images/socks_green.jpg" },
+        { id: 2235, color: "blue", image: "./assets/images/socks_blue.jpg" },
       ],
       cart: 0,
     };
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1;
+    },
+    updateImage(vImage) {
+      this.image = vImage;
+    },
   },
 });
