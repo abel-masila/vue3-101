c313e60cb699369f74e93a2976e53d825361c9fc
Abel Masila <abelmasila@gmail.com>
2020-09-21 23:33:45 +0300

    working on cart

--- main.js
+++ main.js
@@ -3,7 +3,13 @@ const app = Vue.createApp({
     return {
       product: "Boots",
       image: "./assets/images/socks_green.jpg",
-      inventory: 10,
+      inventory: 100,
+      details: ["50% cotton", "30% wool", "20% polyester"],
+      variants: [
+        { id: 2234, color: "green" },
+        { id: 2235, color: "blue" },
+      ],
+      cart: 0,
     };
   },
 });
