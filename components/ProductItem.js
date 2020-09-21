974f04a7dc1a9657f508b837c55cf9e65f8bea96
Abel Masila <abelmasila@gmail.com>
2020-09-22 00:09:19 +0300

    added product component

---
+++ components/ProductItem.js
@@ -0,0 +1,78 @@
+app.component("product-item", {
+  template:
+    /* html */
+    `<div class="product-display">
+    <div class="product-container">
+      <div class="product-image">
+        <img :src="image" alt="item image" />
+      </div>
+      <div class="product-info">
+        <h1>{{ title }}</h1>
+        <h1>{{ product }}</h1>
+        <p v-if="inStock">In Stock</p>
+        <p v-else>Out of Stock</p>
+        <ul>
+          <li v-for="detail in details" :key="detail">{{detail}}</li>
+        </ul>
+        <div
+          v-for="(variant,index) in variants"
+          :key="index"
+          @mouseover="updateVariant(index)"
+          class="color-circle"
+          :style="{backgroundColor:variant.color}"
+        ></div>
+        <button
+          class="button"
+          @click="addToCart"
+          :disabled="!inStock"
+          :class="{disabledButton:!inStock}"
+        >
+          Add to Cart
+        </button>
+      </div>
+    </div>
+  </div>`,
+  data() {
+    return {
+      product: "Boots",
+      selectedVariant: 0,
+      inventory: 100,
+      brand: "Nike",
+      product: "Socks",
+      details: ["50% cotton", "30% wool", "20% polyester"],
+      variants: [
+        {
+          id: 2234,
+          color: "green",
+          image: "./assets/images/socks_green.jpg",
+          qty: 40,
+        },
+        {
+          id: 2235,
+          color: "blue",
+          image: "./assets/images/socks_blue.jpg",
+          qty: 0,
+        },
+      ],
+    };
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1;
+    },
+    updateVariant(index) {
+      this.selectedVariant = index;
+    },
+  },
+  computed: {
+    title() {
+      return this.brand + " " + this.product;
+    },
+    image() {
+      return this.variants[this.selectedVariant].image;
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].qty;
+    },
+  },
+});
--- main.js
+++ main.js
@@ -1,46 +1,8 @@
 const app = Vue.createApp({
   data() {
     return {
-      product: "Boots",
-      selectedVariant: 0,
-      inventory: 100,
-      brand: "Nike",
-      product: "Socks",
-      details: ["50% cotton", "30% wool", "20% polyester"],
-      variants: [
-        {
-          id: 2234,
-          color: "green",
-          image: "./assets/images/socks_green.jpg",
-          qty: 40,
-        },
-        {
-          id: 2235,
-          color: "blue",
-          image: "./assets/images/socks_blue.jpg",
-          qty: 0,
-        },
-      ],
       cart: 0,
     };
   },
-  methods: {
-    addToCart() {
-      this.cart += 1;
-    },
-    updateVariant(index) {
-      this.selectedVariant = index;
-    },
-  },
-  computed: {
-    title() {
-      return this.brand + " " + this.product;
-    },
-    image() {
-      return this.variants[this.selectedVariant].image;
-    },
-    inStock() {
-      return this.variants[this.selectedVariant].qty;
-    },
-  },
+  methods: {},
 });
